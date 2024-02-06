50e4ec8b72027fbcf0b937d928c83fb928b723dc
Roshan Shibu <roshanshibu97@gmail.com>
2024-02-06 23:31:56 +0530

    better focus behavior

--- app/components/DataPoint/DataPoint.jsx
+++ app/components/DataPoint/DataPoint.jsx
@@ -17,25 +17,18 @@ const DataPoint = ({ x, y, index, updateDataPoint, removeDataPoint }) => {
         type="number"
         id="dataX"
         className="dataXInput"
-        value={x}
-        size={20}
+        value={Number(x).toString()}
         onChange={(e) => {
           updateDataPoint(e.target.value, y, index);
-        }}
-        onFocus={(e) => {
-          e.target.select();
         }}
       />
       <input
         type="number"
         id="dataY"
         className="dataYInput"
-        value={y}
+        value={Number(y).toString()}
         onChange={(e) => {
           updateDataPoint(x, e.target.value, index);
-        }}
-        onFocus={(e) => {
-          e.target.select();
         }}
       />
       {index != 0 && (
